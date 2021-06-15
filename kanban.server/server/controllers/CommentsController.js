3ac539578ccf55e89c26fea1ccada022d8837633
Brian Lipsett <lipsettbrian3@gmail.com>
2021-06-15 13:31:25 -0600

    create comments

---
+++ kanban.server/server/controllers/CommentsController.js
@@ -0,0 +1,22 @@
+import { Auth0Provider } from '@bcwdev/auth0provider'
+import { commentsService } from '../services/CommentsService'
+import BaseController from '../utils/BaseController'
+
+export class CommentsController extends BaseController {
+  constructor() {
+    super('api/comments')
+    this.router
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.createComment)
+  }
+
+  async createComment(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      const comment = await commentsService.createComment(req.body)
+      return res.send(comment)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
