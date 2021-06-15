33ad057e083afa5e1a3b7812f166133c4d672fe8
Isaacgonzalez03 <Isaac.albert.g@gmail.com>
2021-06-15 06:22:48 -0600

    dbcontext class

--- kanban.server/server/db/DbContext.js
+++ kanban.server/server/db/DbContext.js
@@ -4,14 +4,15 @@ import { AccountSchema } from '../models/Account'
 import { BoardSchema } from '../models/Board'
 import { CommentSchema } from '../models/Comment'
 import { TaskSchema } from '../models/Task'
+import { ListSchema } from '../models/List'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Boards = mongoose.model('Board', BoardSchema);
-  Comments = mongoose.model('Comment', CommentSchema);
-  Lists = mongoose.model('List', CommentSchema);
+  Lists = mongoose.model('List', ListSchema);
   Tasks = mongoose.model('Task', TaskSchema);
+  Comments = mongoose.model('Comment', CommentSchema);
 }
 
 export const dbContext = new DbContext()
