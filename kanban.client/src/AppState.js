7546022ca884d45ebcba81174d04627435c3a7e1
Isaacgonzalez03 <Isaac.albert.g@gmail.com>
2021-06-16 10:59:52 -0600

    work

--- kanban.client/src/AppState.js
+++ kanban.client/src/AppState.js
@@ -4,5 +4,8 @@ import { reactive } from 'vue'
 export const AppState = reactive({
   user: {},
   account: {},
-  boards: []
+  board: [],
+  list: [],
+  task: [],
+  comment: []
 })
---
+++ kanban.client/src/components/Task.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="task row">
+    <div class="col-4">
+      <div class="card">
+        <h3 class="card-header">
+          <div class="input-group mb-3">
+            <div class="input-group-text">
+              <input class="form-check-input mt-0" type="checkbox" value="">
+            </div>
+            <input type="text" class="form-control" placeholder="task...">
+          </div>
+        </h3>
+        <form @submit.prevent="createComment">
+          <input type="text" placeholder="comment..." class="form-control">
+        </form>
+        <Comment v-for="comment in state.comment" :key="comment.id" :comment="comment" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive } from '@vue/reactivity'
+import { computed } from '@vue/runtime-core'
+import { AppState } from '../AppState'
+
+export default {
+  name: 'Task',
+  setup() {
+    const state = reactive({
+      comment: computed(() => AppState.comment)
+    })
+    return { state }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
